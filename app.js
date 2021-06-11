50f91457ccc59ac4eb250e782457cb060db3a1ae
lelex13 <lelecarballo@gmail.com>
2021-06-10 21:07:39 -0300

    coreccion json

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, './public')));
-app.listen(8000,() => {
+app.use(express.static(path.resolve(__dirname, './public'))); 
+
+/*app.listen(8000,() => {
     console.log('Servidor corriendo en el puerto 8000')
-});
+});*/
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
@@ -18,6 +26,3 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
-/*app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
-})*/
